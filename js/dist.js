3c8973297fd73ba11a9ed1c1d16ebb87e7f09667
Michiaro <michiaro@bk.ru>
2018-05-23 00:08:34 +0500

    add animations, responsibility

--- js/dist.js
+++ js/dist.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var app = new Vue({
   el: "#app",
   data: {
@@ -5,8 +7,8 @@ var app = new Vue({
     isMobile: false,
     isScrolled: false
   },
-  mounted: function() {
-    this.$nextTick(function() {
+  mounted: function mounted() {
+    this.$nextTick(function () {
       window.addEventListener("resize", this.handleResize);
       window.addEventListener("scroll", this.handleScroll);
       this.handleResize();
@@ -45,56 +47,52 @@ var app = new Vue({
         }
       };
 
-      wrapper.addEventListener("mousemove", function(e) {
+      wrapper.addEventListener("mousemove", function (e) {
         var pageX = e.clientX,
-          pageY = e.clientY;
+            pageY = e.clientY;
         for (var key in layers) {
           var depthX = layers[key].depthX;
           var depthY = layers[key].depthY;
-          layers[key].elem.style.transform =
-            "translateX(" +
-            pageX / depthX +
-            "px) translateY(" +
-            pageY / depthY +
-            "px)";
+          layers[key].elem.style.transform = "translateX(" + pageX / depthX + "px) translateY(" + pageY / depthY + "px)";
         }
       });
 
       var collageWrapper = document.getElementById("js-collage-wrapper");
       var collageContainer = document.getElementById("js-collage-container");
-      collageWrapper.addEventListener("mousemove", function(e) {
-        var position = (e.clientX / window.innerWidth * 10) - 5;
-        collageContainer.style.transform = "translateX("+position+"%)"
+      collageWrapper.addEventListener("mousemove", function (e) {
+        var position = e.clientX / window.innerWidth * 10 - 5;
+        collageContainer.style.transform = "translateX(" + position + "%)";
       });
     });
   },
   methods: {
-    handleResize() {
+    handleResize: function handleResize() {
       this.isMobile = window.innerWidth < 1200;
       this.isMenuOpen = window.innerWidth >= 1200;
     },
-    handleScroll() {
+    handleScroll: function handleScroll() {
       this.isScrolled = window.scrollY > 0;
     },
-    toggleMenu() {
+    toggleMenu: function toggleMenu() {
       this.isMenuOpen = !this.isMenuOpen;
     },
-    closeMenu() {
+    closeMenu: function closeMenu() {
       this.isMenuOpen = false;
     },
-    random() {
+    random: function random() {
       return Math.floor(Math.random() * 3) + 1;
     },
+
     submit: function submit(data, url) {
       var self = this;
-      this.post(data, url, function(result) {
+      this.post(data, url, function (result) {
         if (result) {
           for (var field in self.form.data) {
             field.data = "";
           }
           self.form.isAgree = false;
           self.form.isSent = true;
-          setTimeout(function() {
+          setTimeout(function () {
             self.form.isSent = false;
           }, 3e3);
         }
@@ -105,7 +103,7 @@ var app = new Vue({
       xhr.open("POST", url, !0);
       xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
       xhr.send(JSON.stringify(data));
-      xhr.onreadystatechange = function() {
+      xhr.onreadystatechange = function () {
         if (xhr.readyState === 4 && xhr.status === 200) {
           success(xhr.responseText);
         }
@@ -114,36 +112,29 @@ var app = new Vue({
   }
 });
 
-(function() {
-  if (
-    "querySelector" in document &&
-    "addEventListener" in window &&
-    Array.prototype.forEach
-  ) {
-    var smoothScroll = function(anchor, duration) {
+(function () {
+  if ("querySelector" in document && "addEventListener" in window && Array.prototype.forEach) {
+    var smoothScroll = function smoothScroll(anchor, duration) {
       var startLocation = window.pageYOffset;
       var endLocation = anchor.offsetTop;
       var distance = endLocation - startLocation;
       var increments = distance / (duration / 16);
       var stopAnimation;
 
-      var animateScroll = function() {
+      var animateScroll = function animateScroll() {
         window.scrollBy(0, increments);
         stopAnimation();
       };
 
       if (increments >= 0) {
-        stopAnimation = function() {
+        stopAnimation = function stopAnimation() {
           var travelled = window.pageYOffset;
-          if (
-            travelled >= endLocation - increments ||
-            window.innerHeight + travelled >= document.body.offsetHeight
-          ) {
+          if (travelled >= endLocation - increments || window.innerHeight + travelled >= document.body.offsetHeight) {
             clearInterval(runAnimation);
           }
         };
       } else {
-        stopAnimation = function() {
+        stopAnimation = function stopAnimation() {
           var travelled = window.pageYOffset;
           if (travelled <= (endLocation || 0)) {
             clearInterval(runAnimation);
@@ -156,38 +147,30 @@ var app = new Vue({
 
     var scrollToggle = document.querySelectorAll(".js-scroll");
 
-    [].forEach.call(scrollToggle, function(toggle) {
-      toggle.addEventListener(
-        "click",
-        function(e) {
-          e.preventDefault();
+    [].forEach.call(scrollToggle, function (toggle) {
+      toggle.addEventListener("click", function (e) {
+        e.preventDefault();
 
-          var dataID = toggle.getAttribute("href");
-          var dataTarget = document.querySelector(dataID);
-          var dataSpeed = toggle.getAttribute("data-speed");
+        var dataID = toggle.getAttribute("href");
+        var dataTarget = document.querySelector(dataID);
+        var dataSpeed = toggle.getAttribute("data-speed");
 
-          if (dataTarget) {
-            smoothScroll(dataTarget, dataSpeed || 500);
-          }
-        },
-        false
-      );
+        if (dataTarget) {
+          smoothScroll(dataTarget, dataSpeed || 500);
+        }
+      }, false);
     });
   }
 })();
 
 function addClassToElem(el, className) {
-  if (el.classList) el.classList.add(className);
-  else el.className += " " + className;
+  if (el.classList) el.classList.add(className);else el.className += " " + className;
 }
 
 function removeClassFromElem(el, className) {
   if (el.classList) {
     el.classList.remove(className);
   } else {
-    el.className = el.className.replace(
-      new RegExp("(^|\\b)" + className.split(" ").join("|") + "(\\b|$)", "gi"),
-      " "
-    );
+    el.className = el.className.replace(new RegExp("(^|\\b)" + className.split(" ").join("|") + "(\\b|$)", "gi"), " ");
   }
 }
