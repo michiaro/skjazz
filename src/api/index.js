26e7e5f888abf57c8de844a7c631951bdb359aed
dubaua <dubaua@gmail.com>
2018-06-08 18:18:21 +0500

    add api methods

--- src/api/index.js
+++ src/api/index.js
@@ -1,1 +1,12 @@
 /* eslint-disable no-param-reassign */
+const getCollectionByKey = key =>
+  fetch(`/cockpit/api/collections/get/${key}?token=${process.env.API_KEY}`)
+    .then(response => response.json())
+    .then(blob => blob.entries);
+
+const getRegionByKey = key =>
+  fetch(`/cockpit/api/regions/data/${key}?token=${process.env.API_KEY}`)
+    .then(response => response.json())
+    .then(blob => blob);
+
+export default { getCollectionByKey, getRegionByKey };
